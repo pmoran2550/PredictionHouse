6624bd6eade4c9654e9ce22073659e38fe51b27a
pmoran2550 <patmoran@wi.rr.com>
2021-10-27 22:12:14 -0500

    Initial front end checkin

---
+++ PredictionHouseFrontEnd/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
